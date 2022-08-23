d8565b94401406e8654e4edc1be9a127df84a5d4
Friedemann Sommer <social@friedemannsommer.com>
2022-08-23 18:00:01 +0200

    updated formatting

--- benches/tc_v2_string.rs
+++ benches/tc_v2_string.rs
@@ -1,6 +1,8 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use lib_tcstring::TcModelV2;
 use std::convert::TryFrom;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use lib_tcstring::TcModelV2;
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("TCString V2 (core only)", |b| {
